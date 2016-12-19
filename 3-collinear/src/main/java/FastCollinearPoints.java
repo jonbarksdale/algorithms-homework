229ca36ce2d7776974a477b85ec21d9fa57dbda5
Jon Barksdale <jon.barksdale@gmail.com>
2016-12-18 17:34:16 -0700

    Fixing multiple segments on same line be removing dups.

--- 3-collinear/src/main/java/FastCollinearPoints.java
+++ 3-collinear/src/main/java/FastCollinearPoints.java
@@ -21,17 +21,35 @@ public class FastCollinearPoints {
   private void findSegments(Point[] points) {
     // todo: need to filter out segments with matching slopes on the same line.
     // The issue is that we are finding the same line with the subsequent point in line
+
+    /*
+    Alternate algorithm - for each point, subset the array, sort by slope order with a stable sort
+
+    Or just add a remove dup check at the end.
+    dup if slope is same, and it contains any of the points. (slope and intercept are same would work too.)
+     */
+
+    List<Pair> pairs = new ArrayList<>();
+
     for (int i = 0; i < points.length; i++) {
       Point p = points[i];
 
       Map<Double, List<Point>> map = Arrays.stream(points, i, points.length)
           .collect(groupingBy(p::slopeTo));
-      segments = map.values().stream()
+      map.values().stream()
           .filter(points1 -> points1.size() >= 3) // 3, because this wont include the origin
           .map(TreeSet::new)// sort via natural order
-          .map(treeSet -> new LineSegment(p, treeSet.last()))
-          .collect(toCollection(() -> segments));
+          .map(treeSet -> new Pair(p, treeSet.last()))
+          .filter(current -> pairs.stream().noneMatch(existing -> isSameLine(existing, current)))
+          .forEach(pairs::add);
     }
+
+    pairs.stream().map(Pair::toLineSegment).forEach(segments::add);
+  }
+
+  private boolean isSameLine(final Pair first, final Pair last) {
+    return first.slope == last.slope &&
+        (first.a.compareTo(last.a) == 0 || first.b.compareTo(last.b) == 0);
   }
 
   /**
@@ -54,4 +72,22 @@ public class FastCollinearPoints {
   public LineSegment[] segments() {
     return segments.toArray(new LineSegment[0]);
   }                // the line segments
+
+  private class Pair {
+    final Point a;
+    final Point b;
+    final double slope;
+
+    Pair(Point a, Point b) {
+      this.a = a;
+      this.b = b;
+
+      this.slope = a.slopeTo(b);
+    }
+
+    LineSegment toLineSegment() {
+      return new LineSegment(a, b);
+    }
+
+  }
 }
